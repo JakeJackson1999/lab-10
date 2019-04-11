ea9fc464ea3d1ad693887761da9340cc181dc24a
Jake Jackson <jake.r.jackson-1@ou.edu>
2019-04-11 15:39:52 -0500

    finished Shape

--- Shape.java
+++ Shape.java
@@ -5,4 +5,29 @@ import java.awt.Graphics;
 public abstract class Shape implements Drawable
 {
 	
+	protected Point[] location;
+	private Color color;
+	private boolean filled;
+	
+	public Shape(Color color, boolean filled)
+	{
+		this.color = color;
+		this.filled = filled;
+	}
+	
+	public Color getColor()
+	{
+		return color;
+	}
+	
+	public boolean isFilled()
+	{
+		return filled;
+	}
+	
+	public Point[] getLocation()
+	{
+		return location;
+	}
+	
 }
